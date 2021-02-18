d286f6ed78fba719ccb734e3ae7a0a73613e1e9e
Ian Chai <ianchai@mmu.edu.my>
2021-02-18 16:52:12 +0800

    Added some data

--- app/src/main/java/com/lomaikai/recyclerviewexample/MainActivity.java
+++ app/src/main/java/com/lomaikai/recyclerviewexample/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,8 +19,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // LIk the object to the UI element
+        // Link the object to the UI element
         contextRecView=findViewById(R.id.contextRecView);
-
+        // Create array list of contacts
+        ArrayList<Contact> contacts = new ArrayList();
+        contacts.add(new Contact("John Avery Whitaker", "013-124-5540", "https://static.wikia.nocookie.net/the-imagination-station/images/d/d1/John_Whittaker_001.jpg"));
+        contacts.add(new Contact("Connie Kendall", "011-123-5432", "https://static.wikia.nocookie.net/the-imagination-station/images/3/3d/Connie_Kendall_001.jpg"));
+        contacts.add(new Contact("Eugene Meltzner", "010-324-5230", "https://static.wikia.nocookie.net/p__/images/0/08/7CE66103-BCC3-487E-AC6A-AA2F68195C8C.png/revision/latest/scale-to-width-down/180?cb=20180914180349&path-prefix=protagonist"));
+        contacts.add(new Contact("Katrina Shanks", "019-434-2354", "https://www.adventuresinodyssey.com/wp-content/uploads/23_Katrina-Meltsner-1.png"));
+        contacts.add(new Contact("Aubrey Shepherd", "014-484-3454", "https://www.adventuresinodyssey.com/wp-content/uploads/Aubrey-Shepard-Clipped.png"));
+        contacts.add(new Contact("Bernard Walton", "015-555-5354", "https://static.wikia.nocookie.net/adventuresinodyssey/images/4/43/255px-Bernardwalton.png"));
     }
 }
