f610ada5228486aea5a9631a7c6ea62720eab98a
Ian Chai <ianchai@mmu.edu.my>
2021-02-18 15:40:51 +0800

    Added RecyclerView

--- app/src/main/java/com/lomaikai/recyclerviewexample/MainActivity.java
+++ app/src/main/java/com/lomaikai/recyclerviewexample/MainActivity.java
@@ -1,14 +1,22 @@
 package com.lomaikai.recyclerviewexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Create object for Recycler View
+    private RecyclerView contextRecView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // LIk the object to the UI element
+        contextRecView=findViewById(R.id.contextRecView);
+
     }
 }
